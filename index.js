d3b206ed6d619328e92041270496ed88d1567946
akameco <akameco.t@gmail.com>
2015-09-14 19:11:57 +0900

    Update foxtail version

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 'use strict';
 var FoxTail = require('foxtail');
 var Path = require('path');
-var fox = new FoxTail();
+
+var fox = new FoxTail({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  access_token: process.env.ACCESS_TOKEN,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+});
 
 fox.load(Path.resolve(__dirname, 'plugins'));
 fox.run();
