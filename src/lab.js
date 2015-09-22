864e9922b14d51821a1e143b45d027a5dc315228
akameco <akameco.t@gmail.com>
2015-09-23 03:15:06 +0900

    Add lab

---
+++ src/lab.js
@@ -0,0 +1,18 @@
+import request from 'request'
+
+export default fox => {
+  fox.add(res => {
+    if(/^ラボ$/.test(res.text)) {
+      const url = `https://tdu13fi.herokuapp.com/api/v1/count`
+      request(url, (err, response, body) => {
+        const data = JSON.parse(body)
+        const arr = []
+        for(let e in data) {
+          arr.push(`${e}研${data[e]}人`)
+        }
+        console.log(arr)
+        res.reply(arr.join("\n"))
+      })
+    }
+  })
+}
