ab58be1e2d67e3462711d514c3667ef2b387723e
akameco <akameco.t@gmail.com>
2015-09-23 13:29:02 +0900

    Update

--- plugins/lab.js
+++ plugins/lab.js
@@ -1,29 +1,30 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
+Object.defineProperty(exports, '__esModule', {
   value: true
 });
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
 
 var _request = require('request');
 
 var _request2 = _interopRequireDefault(_request);
 
-exports["default"] = function (fox) {
+exports['default'] = function (fox) {
   fox.add(function (res) {
     if (/^ラボ$/.test(res.text)) {
-      var url = "https://tdu13fi.herokuapp.com/api/v1/count";
-      (0, _request2["default"])(url, function (err, response, body) {
+      var url = 'https://tdu13fi.herokuapp.com/api/v1/count';
+      (0, _request2['default'])(url, function (err, response, body) {
         var data = JSON.parse(body);
         var arr = [];
         for (var e in data) {
-          arr.push(e + "研" + data[e] + "人");
+          arr.push(e + '研' + data[e] + '人');
         }
+        console.log(arr);
         res.reply(arr.join("\n"));
       });
     }
   });
 };
 
-module.exports = exports["default"];
+module.exports = exports['default'];
